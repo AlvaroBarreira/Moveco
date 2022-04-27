388b8d69648e74ee4f2c81270409fcac7c99b3d0
Emanuel <emanuelcosntancio.dev@gmail.com>
2022-04-26 22:43:03 -0300

    Working on Navbar

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link } from "react-router-dom";
 import { Item } from './Item/Item';
 import './Navbar.css'
@@ -6,22 +6,76 @@ import { titles } from '../../Utils/Navbar.utils'
 
 
 export const Navbar: React.FC = () => {
+
+
+    const [active, setActive] = useState("nav__menu");
+    const [icon, setIcon] = useState("nav__toggler");
+    
+    const navToggle = () => {
+        if (active === "nav__menu") {
+            setActive("nav__menu nav__active");
+        } else setActive("nav__menu");
+
+        // Icon Toggler
+        if (icon === "nav__toggler") {
+            setIcon("nav__toggler toggle");
+        } else setIcon("nav__toggler");
+    };
+
+
+
+     
     return (
-        <div className="navbar">
-            <div className="navbar-column">
-                <div className="logo">
-                    <Link to="/">
-                        {/* <img className='logo-image' src={logo}></img> */}
-                        Moveco
-                    </Link>
-                </div>
+        <nav className="nav">
+            <a href="#" className="nav__brand">
+                Moveco
+            </a>
+            <ul className={active}>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Home
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        About
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Portfolio
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Skills
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="#" className="nav__link">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+            <div onClick={navToggle} className={icon}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
             </div>
-            <div className="navbar-column links">
-                {titles.map((title) => {
-                    return <Item item={title.item}  />
-                })}
-            </div>
-        </div>
+        </nav>
     );
+
+
 };
+
+
+
+
+
+
+
+
+
+
+
 
