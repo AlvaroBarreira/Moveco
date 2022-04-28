c2dffa5a877544eb8e04eb2249e401c5f9d6e1bc
Emanuel <emanuelcosntancio.dev@gmail.com>
2022-04-28 20:07:34 -0300

    Working on Navbar

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,14 +1,15 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { Item } from './Item/Item';
-import './Navbar.css'
+
 import { titles } from '../../Utils/Navbar.utils'
 
 
 
 
 
-interface PropsItem  {
+interface PropsItem {
     item: string,
     route: string,
     class_name_li: string,
@@ -20,7 +21,7 @@ export const Navbar: React.FC = () => {
 
     const [active, setActive] = useState("nav__menu");
     const [icon, setIcon] = useState("nav__toggler");
-    
+
     const navToggle = () => {
         if (active === "nav__menu") {
             setActive("nav__menu nav__active");
@@ -34,33 +35,36 @@ export const Navbar: React.FC = () => {
 
 
 
-     
+
     return (
-        <nav className="nav">
-            <Link to='#' className="nav__brand">
-                Moveco
-            </Link>
-            <ul className={active}>
+        <div className='div-main-nav'>
 
-                {
-                    titles.map((title: PropsItem) => {
-                        return <Item  title={title} key={title.item} />
-                    })
-               
+            <nav className="nav">
+                <Link to='#' className="nav__brand">
+                    Moveco
+                </Link>
+                <ul className={active}>
 
-                    // array.map((item: PropsItem) => {
-                    //     return <ItemFooter key={item.sentence} item={item} />
-                    // })
+                    {
+                        titles.map((title: PropsItem) => {
+                            return <Item title={title} key={title.item} />
+                        })
 
-                }
-                
-            </ul>
-            <div onClick={navToggle} className={icon}>
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-        </nav>
+
+                        // array.map((item: PropsItem) => {
+                        //     return <ItemFooter key={item.sentence} item={item} />
+                        // })
+
+                    }
+
+                </ul>
+                <div onClick={navToggle} className={icon}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+            </nav>
+        </div>
     );
 
 
