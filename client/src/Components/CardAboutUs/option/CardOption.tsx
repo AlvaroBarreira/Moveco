3c14260d3792fe632a36e946a136205aa4aec8ec
Emanuel <emanuelcosntancio.dev@gmail.com>
2022-05-04 19:16:41 -0300

    Changes on card aboutUs

--- client/src/Components/CardAboutUs/option/CardOption.tsx
+++ client/src/Components/CardAboutUs/option/CardOption.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './CardOption.css'
+
 interface CardOptionProps {
     card: CardOptionObj
 }
@@ -23,15 +24,17 @@ const CardOption: React.FC<CardOptionProps> = ({card}) => {
                         <div className="front__bkg-photo"></div>
                         <div className="front__face-photo"></div>
                         <div className="front__text">
-                            <h3 className="front__text-header">{card.name}</h3>
-                            <p className="front__text-para"><i className="fas fa-map-marker-alt front-icons"></i>{card.surname}</p>
+                            <span className="front__text-header">{card.name}</span>
+                            <span className="front__text-header">{card.surname}</span>
 
                             <span className="front__text-hover">Más sobre mi</span>
                         </div>
                     </div>
                     <div className="back">
                         <div className="social-media-wrapper">
-                            <span>{card.description}</span>
+                            <div className='back-text-container'>
+                            <span className='back-text'>{card.description}</span>
+                            </div>
                             <Link to="#" className="social-icon"><i className="fab fa-linkedin-square" aria-hidden="true"></i></Link>
                             <Link to="#" className="social-icon"><i className="fab fa-instagram" aria-hidden="true"></i></Link>
                         </div>
--- client/src/Utils/CardAboutUs.utils.ts
+++ client/src/Utils/CardAboutUs.utils.ts
@@ -5,64 +5,64 @@ export const cardAboutUs = [
     name: 'Emanuel',
     surname: 'Constancio',
     ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+    description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
 },
-{
-    id: 1,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// {
+//     id: 1,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-},
-{
-    id: 2,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// },
+// {
+//     id: 2,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-},
-{
-    id: 3,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// },
+// {
+//     id: 3,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-},
-{
-    id: 4,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// },
+// {
+//     id: 4,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-},
-{
-    id: 5,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// },
+// {
+//     id: 5,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-}
-,
-{
-    id: 6,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// }
+// ,
+// {
+//     id: 6,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-},
-{
-    id: 7,
-    name: 'Emanuel',
-    surname: 'Constancio',
-    ocupation: 'Software analist',
-    description: 'Lorem insup lorem insup about us so you know how is it'
+// },
+// {
+//     id: 7,
+//     name: 'Emanuel',
+//     surname: 'Constancio',
+//     ocupation: 'Software analist',
+//     description: 'Lorem insup lorem insup about us so you know how is it Lorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup aboutLorem insup lorem insup about'
 
-}
+// }
 ]
