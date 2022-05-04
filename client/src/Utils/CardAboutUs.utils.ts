32753147271386d00f800d2ccb57273df8fd7ba3
Emanuel <emanuelcosntancio.dev@gmail.com>
2022-05-03 23:17:24 -0300

    CardAboutUs util

---
+++ client/src/Utils/CardAboutUs.utils.ts
@@ -0,0 +1,60 @@
+export const cardAboutUs = [
+
+{
+    id: 0,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+},
+{
+    id: 1,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+},
+{
+    id: 2,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+},
+{
+    id: 3,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+},
+{
+    id: 4,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+},
+{
+    id: 5,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+}
+,
+{
+    id: 6,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+},
+{
+    id: 7,
+    name: 'Emanuel',
+    ocupacion: 'Software analist',
+    description: 'Lorem insup lorem insup about us so you know how is it'
+
+}
+]
