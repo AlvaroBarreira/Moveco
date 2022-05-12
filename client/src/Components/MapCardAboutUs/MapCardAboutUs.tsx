97261944e706fd9a787b34f7d51dbb9022999e9a
Emanuel <emanuelcosntancio.dev@gmail.com>
2022-05-12 17:53:42 -0300

    Title on cards

--- client/src/Components/MapCardAboutUs/MapCardAboutUs.tsx
+++ client/src/Components/MapCardAboutUs/MapCardAboutUs.tsx
@@ -4,19 +4,23 @@ import { CardAboutUs } from '../CardAboutUs/CardAboutUs';
 import './MapCardAboutUs.css'
 
 interface MapCardAboutUsProps {
-   
+
 }
 
-export const MapCardAboutUs: React.FC<MapCardAboutUsProps> = ({  }) => {
+export const MapCardAboutUs: React.FC<MapCardAboutUsProps> = ({ }) => {
     return (
+        <>
+        <span className='nosotros-span'>Nosotros</span>
         <div className='aboutUsContainer'>
+
+          
             {
                 cardAboutUs.map(card => {
                     return <CardAboutUs key={card.id} card={card} />
                 })
             }
         </div>
+        </>
     );
 };
 
- 
